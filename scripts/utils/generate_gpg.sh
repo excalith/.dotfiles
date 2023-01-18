fda6729d5838bc780777da9ec54f0dbca938cb15
Can Cellek <excalith@users.noreply.github.com>
2023-01-18 18:47:44 +0300

    Fix GPG setup

--- scripts/utils/generate_gpg.sh
+++ scripts/utils/generate_gpg.sh
@@ -7,7 +7,6 @@
 . "$HOME/.dotfiles/scripts/utils/utils.sh"
 
 generate_gpg_key() {
-    # Read required information
     ask "GPG Username: "
     local gpg_name="$(get_answer)"
 
@@ -15,7 +14,7 @@ generate_gpg_key() {
     local gpg_mail="$(get_answer)"
 
     # Generate gpg keys
-    gpg --quick-generate-key "$gpg_name ($uname) <$gpg_mail>" rsa4096 never
+    gpg --quick-generate-key "$gpg_name ($uname) <$gpg_mail>" rsa4096 default never
 }
 
 register_to_shell() {
@@ -34,21 +33,21 @@ register_to_shell() {
 
 export_gpg() {
     # Read GPG ID
-    local gpgId=gpg --list-secret-keys --with-colons 2> /dev/null \
-            | grep '^sec:' | cut --delimiter ':' --fields 5
+    gpgId=$(gpg --list-secret-keys --with-colons | grep '^sec:' | cut --delimiter ':' --fields 5)
 
     # Add GPG ID to git config
-    echo "	signingkey = '$gpgId'" >> ~/.config/git/config.local
+    echo "	signingkey = $gpgId" >> ~/.config/git/config.local
 
     # Add GPG Sign function to commit
-    printf "[commit]\n	gpgsign = true" >> ~/.config/git/config.local
-    printf "[gpg]\n	program = gpg" >> ~/.config/git/config.local
+    printf "[commit]\n	gpgsign = true\n[gpg]\n	program = gpg" >> ~/.config/git/config.local
 
     # Copy GPG Key to clipboard
     if cmd_exists "pbcopy"; then
-        gpg --armor --export $gpgId | pbcopy | echo 'GPG Public Key copied to pasteboard.'
+        gpg --armor --export $gpgId | pbcopy
+        print_result $? "GPG Public Key copied to pasteboard."
     elif cmd_exists "xclip"; then
-        gpg --armor --export $gpgId | xclip | echo 'GPG Public Key copied to pasteboard.'
+        gpg --armor --export $gpgId | xclip -sel clip
+        print_result $? "GPG Public Key copied to pasteboard."
     else
         print_warning "Please copy your GPG key with 'gpg --armor --export $gpgId' command"
     fi
