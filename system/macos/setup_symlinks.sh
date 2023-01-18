cde7fd9f210747d827d62e6eb6eda9d8060a08a4
Can Cellek <excalith@users.noreply.github.com>
2023-01-18 17:10:36 +0300

    Change pretty ping fish function

--- system/macos/setup_symlinks.sh
+++ system/macos/setup_symlinks.sh
@@ -27,7 +27,7 @@ symlink ~/.dotfiles/config/fish/bindings.fish ~/.config/fish/bindings.fish
 
 symlink ~/.dotfiles/config/fish/functions/fcd.fish ~/.config/fish/functions/fcd.fish
 symlink ~/.dotfiles/config/fish/functions/mkcd.fish ~/.config/fish/functions/mkcd.fish
-symlink ~/.dotfiles/config/fish/functions/pingpretty.fish ~/.config/fish/functions/pingpretty.fish
+symlink ~/.dotfiles/config/fish/functions/pping.fish ~/.config/fish/functions/pping.fish
 
 symlink ~/.dotfiles/config/fish/theme/excalith.fish ~/.config/fish/theme/excalith.fish
 
