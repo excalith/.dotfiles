e9eeeecc348e582b7e890221d8a4ca4d5e4e6337
Can Cellek <can.cellek@gmail.com>
2023-01-24 22:20:32 +0300

    Add fasd and aliases

--- config/bash/aliases.bash
+++ config/bash/aliases.bash
@@ -30,3 +30,12 @@ alias locip="sudo ifconfig | grep -Eo 'inet (addr:)?([0-9]*\\.){3}[0-9]*' | grep
 # Update Neovim NVChad
 alias nvupdate="nvim -c \"NvChadUpdate\""
 
+# fasd Aliases
+alias a='fasd -a'        # any
+alias s='fasd -si'       # show / search / select
+alias d='fasd -d'        # directory
+alias f='fasd -f'        # file
+alias sd='fasd -sid'     # interactive directory selection
+alias sf='fasd -sif'     # interactive file selection
+alias z='fasd_cd -d'     # cd, same functionality as j in autojump
+alias zz='fasd_cd -d -i' # cd with interactive selection
