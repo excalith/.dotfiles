6997deb3d836674b2dd8af820f6112c8aba3be66
Can Cellek <can.cellek@proton.me>
2023-12-04 18:12:10 +0300

    Remove  dotfile main reset while initializing

--- scripts/utils/init_dotfile_repo.sh
+++ scripts/utils/init_dotfile_repo.sh
@@ -26,28 +26,6 @@ initialize_git_repository() {
             "git init && git remote add origin $GIT_ORIGIN" \
             "Initialize git repository"
     fi
-
-    # Try to connect to repository
-    print_warning "If you haven't added your SSH into GitHub, please do before this step or skip."
-    ask_for_confirmation "Do you want to update the content of dotfiles from repository?"
-    if answer_is_yes; then
-
-        ssh -T git@github.com &> /dev/null
-        if [ $? -ne 0 ]; then
-            print_title "Update dotfiles"
-
-            git fetch --all 1> /dev/null \
-                && git reset --hard origin/main 1> /dev/null \
-                && git checkout main &> /dev/null \
-                && git clean -fd 1> /dev/null
-
-            print_result $? "Update dotfiles content"
-        else
-            print_warning "Github rejected connection. You may want to check your SSH configuration."    
-        fi
-    else
-        print_warning "Cannot update dotfiles content without GitHub SSH setup."    
-    fi
 }
 
 main() {
